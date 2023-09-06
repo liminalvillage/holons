a037732d687180415e38217c5f2f82a7a74cad09
Roberto Valenti <robbyvalenti@gmail.com>
2023-09-06 18:16:15 +0200

    Fixed case sensitive classes

--- Quests.js
+++ Quests.js
@@ -164,7 +164,7 @@ export default class Quests {
                     notifyChats = notifyChats.notify
 
                 if (notifyChats && notifyChats.length > 0) {
-                    let id = ''+quest.chat*2+quest._id
+                    let id = ''+quest.chat*2+quest._id //*2 is a hack not to return similar indexes
                     let fedinfo = federationDB.get(id)[0]
                     console.log(fedinfo)
                     if (!fedinfo || fedinfo == ''|| fedinfo == undefined)
