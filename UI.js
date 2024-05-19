dc5f18acf0a3d8140d197da7dc55b9f9d9db36e3
Roberto Valenti <robbyvalenti@gmail.com>
2024-05-20 00:51:14 +0200

    Fixed quest table and buy command crashing

--- UI.js
+++ UI.js
@@ -351,8 +351,8 @@ class UI {
     for (let i = 0; i < quests.length; i++) {
       const quest = quests[i]
       const row = `<tr>
-          <th scope="row">${i + 1}</th>
-          <th>${quest.id}</th>
+          <th scope="row">${quest.id}</th>
+          <th>${quest.title}</th>
           <th>${quest.initiator.first_name}</th>
           <th>${quest.participants ? quest.participants.length : quest.users.length}</th>
           <th>${quest.appreciation.length}</th>
