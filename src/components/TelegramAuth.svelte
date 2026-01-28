5b23a7241c2705ebb3823f653b699fd07bc77d73
Roberto Valenti <robbyvalenti@gmail.com>
2026-01-28 11:09:28 +0100

    Use env variable for Telegram bot username in login widget

--- src/components/TelegramAuth.svelte
+++ src/components/TelegramAuth.svelte
@@ -121,7 +121,7 @@
 			// Create script element for Telegram Login Widget
 			const script = document.createElement('script');
 			script.src = 'https://telegram.org/js/telegram-widget.js?22';
-			script.setAttribute('data-telegram-login', 'HolonsBot');
+			script.setAttribute('data-telegram-login', import.meta.env.VITE_TELEGRAM_BOT_USERNAME || 'HolonsBot');
 			script.setAttribute('data-size', 'large');
 			script.setAttribute('data-onauth', 'onTelegramAuth(user)');
 			script.setAttribute('data-request-access', 'write');
